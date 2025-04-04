d5d0fd86c1684f5a4c0aac4cc7962afe9aeea3cc
Augusto Pasquali de Lima <augusto.pasqualidelima@gmail.com>
2025-04-04 11:06:52 -0300

    Cria o projeto

---
+++ produtos/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class Categoria(models.Model):
+    nome = models.CharField(max_length=100)
+
+
+class Produto(models.Model):
+    nome = models.CharField(max_length=200)
+    preco = models.DecimalField(max_digits=10, decimal_places=2)
+    descricao = models.TextField()
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
